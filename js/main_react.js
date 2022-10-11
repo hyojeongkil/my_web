50c892af818feece5179d0bb1241eaf96f7fea84
hyojeongkil <110511790+hyojeongkil@users.noreply.github.com>
2022-10-11 21:28:17 +0900

    Update main_react.js

--- js/main_react.js
+++ js/main_react.js
@@ -80,10 +80,10 @@ class MainComponent extends React.Component {
 
                     <section id="content">
                         <ul class="banner">
-                            <li><a href="#"><img src="img/banner1.jpg" alt="" /></a></li>
-                            <li><a href="#"><img src="img/banner2.jpg" alt="" /></a></li>
-                            <li><a href="#"><img src="img/banner3.jpg" alt="" /></a></li>
-                            <li><a href="#"><img src="img/banner5.jpg" alt="" /></a></li>
+                            <li><a href="#"><img src="img/banner1.jpg" alt="미니멀리스트 뷰티컬렉션" /></a></li>
+                            <li><a href="#"><img src="img/banner2.jpg" alt="시몬스 컬렉션" /></a></li>
+                            <li><a href="#"><img src="img/banner3.jpg" alt="KENOSHA & HOME" /></a></li>
+                            <li><a href="#"><img src="img/banner5.jpg" alt="BEAUTYREST HENRY" /></a></li>
                         </ul>
                     </section>
 
@@ -103,7 +103,7 @@ class MainComponent extends React.Component {
                                 <div class="contents">
                                     <div class="inner">
                                         <div class="description">
-                                            <img src="img/collect_1.jpg" alt="" />
+                                            <img src="img/collect_1.jpg" alt="POKET SPRING, 포켓스프링은 1870년부터 한 세기 반 동안 '기본에 충실하자'는 원칙을 고집해 온 장인정신의 집약체입니다. 그 누구도 모방할 수 없는 시몬스만의 흔들리지 않는 편안함을 만듭니다." />
                                         </div>
                                     </div>
                                 </div>
@@ -118,7 +118,7 @@ class MainComponent extends React.Component {
                                 <div class="contents">
                                     <div class="inner">
                                         <div class="description">
-                                            <img src="img/collect_2.jpg" alt="" />
+                                            <img src="img/collect_2.jpg" alt="The Sleeping Beauty, 시몬스만의 독자적인 기술로 완성된 포켓스프링, 흔들리지 않는 편안함, 그 아름다운 휴식 beautyrest" />
                                         </div>
                                     </div>
                                 </div>
@@ -133,7 +133,7 @@ class MainComponent extends React.Component {
                                 <div class="contents">
                                     <div class="inner">
                                         <div class="description">
-                                            <img src="img/collect_3.jpg" alt="" />
+                                            <img src="img/collect_3.jpg" alt="N + 32 = N32, N32는 라이프스타일 컬렉션입니다. 일상의 휴식과 섬세한 감각이 전에 없던 새로운 하루, 32일을 선사합니다. N32와 함께하는 당신의 새로운 하루" />
                                         </div>
                                     </div>
                                 </div>
@@ -148,7 +148,7 @@ class MainComponent extends React.Component {
                                 <div class="contents">
                                     <div class="inner">
                                         <div class="description">
-                                            <img src="img/collect_4.jpg" alt="" />
+                                            <img src="img/collect_4.jpg" alt="KENOSHA FURNITURE1" />
                                         </div>
                                     </div>
                                 </div>
@@ -163,7 +163,7 @@ class MainComponent extends React.Component {
                                 <div class="contents">
                                     <div class="inner">
                                         <div class="description">
-                                            <img src="img/collect_5.jpg" alt="" />
+                                            <img src="img/collect_5.jpg" alt="KENOSHA FURNITURE2" />
                                         </div>
                                     </div>
                                 </div>
@@ -185,15 +185,15 @@ class MainComponent extends React.Component {
                         </div>
                         <div class="width-scrollbox">
                             <ul class="collectbox">
-                                <li><a href="#"><img src="img/mattress1.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress2.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress3.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress4.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress5.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress6.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress7.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress8.jpg" alt="" /></a></li>
-                                <li><a href="#"><img src="img/mettress9.jpg" alt="" /></a></li>
+                                <li><a href="#"><img src="img/mattress1.jpg" alt="mettress img1" /></a></li>
+                                <li><a href="#"><img src="img/mettress2.jpg" alt="mettress img2" /></a></li>
+                                <li><a href="#"><img src="img/mettress3.jpg" alt="mettress img3" /></a></li>
+                                <li><a href="#"><img src="img/mettress4.jpg" alt="mettress img4" /></a></li>
+                                <li><a href="#"><img src="img/mettress5.jpg" alt="mettress img5" /></a></li>
+                                <li><a href="#"><img src="img/mettress6.jpg" alt="mettress img6" /></a></li>
+                                <li><a href="#"><img src="img/mettress7.jpg" alt="mettress img7" /></a></li>
+                                <li><a href="#"><img src="img/mettress8.jpg" alt="mettress img8" /></a></li>
+                                <li><a href="#"><img src="img/mettress9.jpg" alt="mettress img9" /></a></li>
                             </ul>
                         </div>
 
@@ -227,19 +227,19 @@ class MainComponent extends React.Component {
                         </div>
                         <div class="cont-wrap cf">
                             <div class="first-box">
-                                <img src="img/HERITAGE1.jpg" alt="" />
+                                <img src="img/HERITAGE1.jpg" alt="Comfort" />
                                 <span>Comfort<br />
                                     편안함의 원칙을 지켜나갑니다.
                                 </span>
                             </div>
                             <div class="second-box">
-                                <img src="img/HERITAGE2.jpg" alt="" />
+                                <img src="img/HERITAGE2.jpg" alt="Eco-friendly" />
                                 <span>Eco-friendly<br />
                                     다음세대를 생각합니다
                                 </span>
                             </div>
                             <div class="third-box">
-                                <img src="img/HERITAGE3.jpg" alt="" />
+                                <img src="img/HERITAGE3.jpg" alt="Science & Craftmanship" />
                                 <span>Science &#38; Craftmanship<br />
                                     품질에 대한 고집으로 기본원칙을 지킵니다.</span>
                             </div>
